268597c3a7011548ce8e275b8c50764232112963
Jakub Fedyczak <jakub@fedyczak.net>
2016-04-02 11:55:01 +0200

    Further development

--- src/client/app.js
+++ src/client/app.js
@@ -1,1 +1,56 @@
-document.write('test')
+import Rx from 'rx'
+import Cycle from '@cycle/core'
+import { makeDOMDriver, div, h1, ul, li, a, button } from '@cycle/dom'
+import { makeHistoryDriver } from '@cycle/history'
+import { useQueries, createHistory } from 'history'
+
+const main = (sources) => {
+	const history$ = sources.history
+	const aClick$ = sources.DOM.select('a')
+		.events('click')
+		.do(ev => {
+			ev.preventDefault()
+		})
+		.map(ev => ev.target.pathname)
+	const buttonClick$ = sources.DOM.select('button').events('click')
+		.startWith(0)
+		.scan((x, y) => x + 1)
+
+	// stan:
+	const state$ = history$.combineLatest(
+		buttonClick$,
+		(h, b) => {
+			return {
+				gdziejestem: h.pathname,
+				licznik: b
+			}
+		})
+
+	// widok:
+	const vtree$ = state$.
+		map(stan => {
+			return div([
+				h1(`Jestem tu: ${stan.gdziejestem}`),
+				ul([
+					li([
+						a({href:'/'}, 'główna')
+					]),
+					li([
+						a({href:'/login'}, 'logowanie')
+					]),
+				]),
+				button('.button', `${stan.licznik}`)
+			])
+		})
+	return {
+		DOM: vtree$,
+		history: aClick$
+	}
+}
+
+const drivers = {
+	DOM: makeDOMDriver("#app"),
+	history: makeHistoryDriver(createHistory())
+}
+
+Cycle.run(main, drivers)
--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,6 @@
 import Rx from 'rx'
 import http from 'http'
-import fs from 'fs'
+import send from 'send'
 
 let publicFiles = []
 
@@ -9,51 +9,13 @@ let request$ = Rx.Observable
 	.fromEvent(srv, 'request', (req, res) => { 
 		return {req, res, url: req.url, method: req.method} 
 	})
-	.map(f => {
-		f.url = f.url === '/' ? '/index.html' : f.url
-		return f
-	})
 	.subscribe(r => {
-		if (publicFiles.indexOf(r.url) >= 0) {
-			r.res.writeHead(200, {
-				'Content-Type': 'text'
-			})
-			fs.createReadStream(`${__dirname}/../../public${r.url}`)
-				.pipe(r.res)
+		if (false) {
 		} else {
-			r.res.writeHead(404)
-			r.res.end()
-			console.log(`not found ${r.url}`);
+			send(r.req, r.url, {
+				root: `${__dirname}/../../public`,
+			}).pipe(r.res)
 		}
 	})
 
-const filesInDir = Rx.Observable.fromNodeCallback(fs.readdir)
-const statFile = Rx.Observable.fromNodeCallback(fs.stat)
-
-const listFiles = (prefix, dir = '') => {
-	const file$ = filesInDir(`${prefix}/${dir}`)
-		.flatMap(file => file)
-		.filter(file => !file.startsWith('.'))
-	const isDir$ = file$
-		.map(file => statFile(`${prefix}/${dir}/${file}`))
-		.flatMap(file => file)
-		.map(file => file.isDirectory())
-	return file$
-		.zip(isDir$, (file, isDir) => {return {file, isDir}})
-		.map(f => {
-			if (f.isDir) {
-				return listFiles(prefix, `${dir}/${f.file}`)
-			}
-			return Rx.Observable.return(`${dir}/${f.file}`)
-		})
-		.flatMap(file => file)
-}
-
-listFiles('public')
-	.toArray()
-	.subscribe(list => {
-		publicFiles = list
-		console.log(publicFiles);
-		srv.listen(3001)
-	})
-
+srv.listen(3001)
